771b945488cf7c75adc39ab3a21ff8d2fd589214
Philipp Arndt <philipp.arndt@vector.com>
2025-04-29 06:45:33 +0200

    feat: check log level

--- logger.go
+++ logger.go
@@ -1,103 +1,122 @@
 package logger
 
 import (
-	"fmt"
-	"log"
-	"os"
-	"strings"
-	"time"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+    "time"
 )
 
 var customLogger *log.Logger
 
 const (
-	traceLevel int = 5
-	debugLevel int = 4
-	infoLevel  int = 3
-	warnLevel  int = 2
-	errorLevel int = 1
-	panicLevel int = 0
+    traceLevel int = 5
+    debugLevel int = 4
+    infoLevel  int = 3
+    warnLevel  int = 2
+    errorLevel int = 1
+    panicLevel int = 0
 )
 
 var logLevel = infoLevel
 
 var (
-	red    = "\033[31m"
-	yellow = "\033[33m"
-	nc     = "\033[0m"
-	purple = "\033[35m"
-	gray   = "\033[37m"
+    red    = "\033[31m"
+    yellow = "\033[33m"
+    nc     = "\033[0m"
+    purple = "\033[35m"
+    gray   = "\033[37m"
 )
 
 func init() {
-	customLogger = log.New(os.Stdout, "", 0)
+    customLogger = log.New(os.Stdout, "", 0)
 
-	if os.Getenv("NO_COLOR") != "" {
-		red = ""
-		yellow = ""
-		nc = ""
-		purple = ""
-		gray = ""
-	}
+    if os.Getenv("NO_COLOR") != "" {
+        red = ""
+        yellow = ""
+        nc = ""
+        purple = ""
+        gray = ""
+    }
 }
 
 func logMessage(severity string, color string, message string, a ...any) {
-	var level = fmt.Sprintf("%s[%s]%s", color, strings.ToUpper(severity), nc)
-	var timedate = time.Now().Format("2006-01-02T15:04:05 MST")
-	var data = ""
-	if len(a) > 0 {
-		data = fmt.Sprintf(" %s%s%s", gray, a, nc)
-	}
+    var level = fmt.Sprintf("%s[%s]%s", color, strings.ToUpper(severity), nc)
+    var timedate = time.Now().Format("2006-01-02T15:04:05 MST")
+    var data = ""
+    if len(a) > 0 {
+        data = fmt.Sprintf(" %s%s%s", gray, a, nc)
+    }
 
-	customLogger.Printf("%s %s %s%s\n", timedate, level, message, data)
+    customLogger.Printf("%s %s %s%s\n", timedate, level, message, data)
 }
 
 func Trace(message string, a ...any) {
-	if logLevel >= traceLevel {
-		logMessage("trace", gray, message, a...)
-	}
+    if logLevel >= traceLevel {
+        logMessage("trace", gray, message, a...)
+    }
 }
 
 func Debug(message string, a ...any) {
-	if logLevel >= debugLevel {
-		logMessage("debug", purple, message, a...)
-	}
+    if logLevel >= debugLevel {
+        logMessage("debug", purple, message, a...)
+    }
 }
 
 func Info(message string, a ...any) {
-	if logLevel >= infoLevel {
-		logMessage("info", nc, message, a...)
-	}
+    if logLevel >= infoLevel {
+        logMessage("info", nc, message, a...)
+    }
 }
 
 func Warn(message string, a ...any) {
-	if logLevel >= warnLevel {
-		logMessage("warn", yellow, message, a...)
-	}
+    if logLevel >= warnLevel {
+        logMessage("warn", yellow, message, a...)
+    }
 }
 
 func Error(message string, a ...any) {
-	logMessage("error", red, message, a...)
+    logMessage("error", red, message, a...)
 }
 
 func Panic(message string, a ...any) {
-	logMessage("panic", red, message, a...)
-	panic(message)
+    logMessage("panic", red, message, a...)
+    panic(message)
 }
 
 func SetLevel(level string) {
-	switch strings.ToLower(level) {
-	case "trace":
-		logLevel = traceLevel
-	case "debug":
-		logLevel = debugLevel
-	case "info":
-		logLevel = infoLevel
-	case "warn":
-		logLevel = warnLevel
-	case "error":
-		logLevel = errorLevel
-	case "panic":
-		logLevel = panicLevel
-	}
+    switch strings.ToLower(level) {
+    case "trace":
+        logLevel = traceLevel
+    case "debug":
+        logLevel = debugLevel
+    case "info":
+        logLevel = infoLevel
+    case "warn":
+        logLevel = warnLevel
+    case "error":
+        logLevel = errorLevel
+    case "panic":
+        logLevel = panicLevel
+    }
+}
+
+func IsLevelEnabled(level string) bool {
+    switch strings.ToLower(level) {
+    case "trace":
+        return logLevel >= traceLevel
+    case "debug":
+        return logLevel >= debugLevel
+    case "info":
+        return logLevel >= infoLevel
+    case "warn":
+        return logLevel >= warnLevel
+    case "error":
+        return logLevel >= errorLevel
+    case "panic":
+        return logLevel >= panicLevel
+    default:
+        return false
+    }
 }
--- logger_test.go
+++ logger_test.go
@@ -1,38 +1,56 @@
 package logger
 
 import (
-	"github.com/stretchr/testify/assert"
-	"testing"
+    "github.com/stretchr/testify/assert"
+    "testing"
 )
 
 func TestInfo(t *testing.T) {
-	Info("Test")
-	Info("Test", "with", "data")
+    Info("Test")
+    Info("Test", "with", "data")
 }
 
 func TestTrace(t *testing.T) {
-	SetLevel("trace")
-	Trace("Test")
-	Trace("Test", "with", "data")
+    SetLevel("trace")
+    Trace("Test")
+    Trace("Test", "with", "data")
 }
 
 func TestDebug(t *testing.T) {
-	SetLevel("debug")
-	Debug("Test")
-	Debug("Test", "with", "data")
+    SetLevel("debug")
+    Debug("Test")
+    Debug("Test", "with", "data")
 }
 
 func TestWarn(t *testing.T) {
-	Warn("Test")
-	Warn("Test", "with", "data")
+    Warn("Test")
+    Warn("Test", "with", "data")
 }
 
 func TestError(t *testing.T) {
-	Error("Test")
-	Error("Test", "with", "data")
+    Error("Test")
+    Error("Test", "with", "data")
 }
 
 func TestPanic(t *testing.T) {
-	assert.Panics(t, func() { Panic("Test") })
-	assert.Panics(t, func() { Panic("Test", "with", "data") })
+    assert.Panics(t, func() { Panic("Test") })
+    assert.Panics(t, func() { Panic("Test", "with", "data") })
+}
+
+func TestIsLevelEnabled(t *testing.T) {
+    SetLevel("error")
+    assert.False(t, IsLevelEnabled("trace"))
+    assert.False(t, IsLevelEnabled("debug"))
+    assert.False(t, IsLevelEnabled("info"))
+    assert.False(t, IsLevelEnabled("warn"))
+    assert.True(t, IsLevelEnabled("error"))
+    assert.True(t, IsLevelEnabled("panic"))
+    
+    SetLevel("debug")
+    assert.False(t, IsLevelEnabled("trace"))
+    assert.True(t, IsLevelEnabled("debug"))
+    assert.True(t, IsLevelEnabled("info"))
+    assert.True(t, IsLevelEnabled("warn"))
+    assert.True(t, IsLevelEnabled("error"))
+    assert.True(t, IsLevelEnabled("panic"))
 }
