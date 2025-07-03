427dcacd967a2570251372b0bcce9833312193bd
Philipp Arndt <philipp.arndt@vector.com>
2025-07-03 15:33:55 +0200

    feat: set log destination

--- logger_test.go
+++ logger_test.go
@@ -1,56 +1,66 @@
 package logger
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"github.com/stretchr/testify/assert"
+	"strings"
+	"testing"
 )
 
 func TestInfo(t *testing.T) {
-    Info("Test")
-    Info("Test", "with", "data")
+	Info("Test")
+	Info("Test", "with", "data")
 }
 
 func TestTrace(t *testing.T) {
-    SetLevel("trace")
-    Trace("Test")
-    Trace("Test", "with", "data")
+	SetLevel("trace")
+	Trace("Test")
+	Trace("Test", "with", "data")
 }
 
 func TestDebug(t *testing.T) {
-    SetLevel("debug")
-    Debug("Test")
-    Debug("Test", "with", "data")
+	SetLevel("debug")
+	Debug("Test")
+	Debug("Test", "with", "data")
 }
 
 func TestWarn(t *testing.T) {
-    Warn("Test")
-    Warn("Test", "with", "data")
+	Warn("Test")
+	Warn("Test", "with", "data")
 }
 
 func TestError(t *testing.T) {
-    Error("Test")
-    Error("Test", "with", "data")
+	Error("Test")
+	Error("Test", "with", "data")
 }
 
 func TestPanic(t *testing.T) {
-    assert.Panics(t, func() { Panic("Test") })
-    assert.Panics(t, func() { Panic("Test", "with", "data") })
+	assert.Panics(t, func() { Panic("Test") })
+	assert.Panics(t, func() { Panic("Test", "with", "data") })
 }
 
 func TestIsLevelEnabled(t *testing.T) {
-    SetLevel("error")
-    assert.False(t, IsLevelEnabled("trace"))
-    assert.False(t, IsLevelEnabled("debug"))
-    assert.False(t, IsLevelEnabled("info"))
-    assert.False(t, IsLevelEnabled("warn"))
-    assert.True(t, IsLevelEnabled("error"))
-    assert.True(t, IsLevelEnabled("panic"))
-    
-    SetLevel("debug")
-    assert.False(t, IsLevelEnabled("trace"))
-    assert.True(t, IsLevelEnabled("debug"))
-    assert.True(t, IsLevelEnabled("info"))
-    assert.True(t, IsLevelEnabled("warn"))
-    assert.True(t, IsLevelEnabled("error"))
-    assert.True(t, IsLevelEnabled("panic"))
+	SetLevel("error")
+	assert.False(t, IsLevelEnabled("trace"))
+	assert.False(t, IsLevelEnabled("debug"))
+	assert.False(t, IsLevelEnabled("info"))
+	assert.False(t, IsLevelEnabled("warn"))
+	assert.True(t, IsLevelEnabled("error"))
+	assert.True(t, IsLevelEnabled("panic"))
+
+	SetLevel("debug")
+	assert.False(t, IsLevelEnabled("trace"))
+	assert.True(t, IsLevelEnabled("debug"))
+	assert.True(t, IsLevelEnabled("info"))
+	assert.True(t, IsLevelEnabled("warn"))
+	assert.True(t, IsLevelEnabled("error"))
+	assert.True(t, IsLevelEnabled("panic"))
+}
+
+func TestLogTo(t *testing.T) {
+	defer LogTo(nil)
+
+	var buf strings.Builder
+	LogTo(&buf)
+	Info("Test log to writer")
+	assert.Contains(t, buf.String(), "Test log to writer")
 }
